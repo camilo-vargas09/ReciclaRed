721f0a61e4e17991667a1129b44a61d34095b9d7
Camilo_Vargas09 <vargascardenaskevincamilo@gmail.com>
2026-09-09 16:44:59 -0500

    enlace a la BD

---
+++ ReciclaRed/src/main/java/com/example/ReciclaRed/ReciclaRedApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReciclaRed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclaRedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclaRedApplication.class, args);
+	}
+
+}
